2aedfa63523b74e4bf01d64bd0f37bea077626ee
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-21 23:09:23 +0700

    handle sign up email and password in sign up fragment

--- app/src/main/java/com/example/movieapp/view/SignUpFragment.java
+++ app/src/main/java/com/example/movieapp/view/SignUpFragment.java
@@ -1,7 +1,4 @@
 package com.example.movieapp.view;
-
-import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,18 +13,12 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.movieapp.databinding.FragmentSignUpBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class SignUpFragment extends Fragment {
     private FragmentSignUpBinding binding;
     private FirebaseAuth firebaseAuth;
-    private FirebaseUser currentUser;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -44,19 +35,9 @@ public class SignUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentSignUpBinding.inflate(inflater, container, false);
 
-        binding.backIntroBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hideFragment();
-            }
-        });
+        binding.backIntroBtn.setOnClickListener(v -> hideFragment());
 
-        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signUpEmailAndPwd();
-            }
-        });
+        binding.signUpBtn.setOnClickListener(v -> signUpEmailAndPwd());
 
         return binding.getRoot();
     }
@@ -74,24 +55,17 @@ public class SignUpFragment extends Fragment {
         if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd) && !TextUtils.isEmpty(pwd2)){
             if (pwd.compareTo(pwd2) == 0){
                 firebaseAuth.createUserWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()){
-                                    Toast.makeText(requireContext(), "Sign up an account is successful", Toast.LENGTH_SHORT).show();
-                                    currentUser = firebaseAuth.getCurrentUser();
-                                    hideFragment();
-                                }
+                        .addOnCompleteListener(requireActivity(), task -> {
+                            if (task.isSuccessful()){
+                                Toast.makeText(requireContext(), "Sign up an account is successful", Toast.LENGTH_SHORT).show();
+                                hideFragment();
                             }
                         })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                         });
             } else {
-                Toast.makeText(requireContext(), "Passwords are not the same!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), "Incorrect password. Please try again.", Toast.LENGTH_SHORT).show();
             }
         } else {
             Toast.makeText(requireContext(), "No Empty Fields are Allowed!", Toast.LENGTH_SHORT).show();

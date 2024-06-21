95297dbecbbaf37b9c5ce0f67778606e1976e2b4
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-21 23:33:58 +0700

    handle move to home launcher activity

--- app/src/main/java/com/example/movieapp/view/MainActivity.java
+++ app/src/main/java/com/example/movieapp/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.movieapp.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -147,12 +146,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void moveToHomeLauncherActivity() {
-        binding.moveToHomeLauncher.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeLauncherActivity.class);
-                startActivity(intent);
-            }
+        binding.moveToHomeLauncher.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, HomeLauncherActivity.class);
+            startActivity(intent);
         });
     }
 }

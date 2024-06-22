b0d2e1c9d54c381345a5c61a2124f7310585e157
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-23 05:37:11 +0700

    create slider item model

---
+++ app/src/main/java/com/example/movieapp/model/SliderItem.java
@@ -0,0 +1,61 @@
+package com.example.movieapp.model;
+
+public class SliderItem {
+    private String age;
+    private String genre;
+    private String image;
+    private String name;
+    private String time;
+    private String year;
+
+    public SliderItem() {
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}

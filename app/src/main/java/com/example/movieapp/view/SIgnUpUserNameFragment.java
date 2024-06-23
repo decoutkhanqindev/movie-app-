c4462bd3917f29db9cbddb922f82ccf175123e50
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-23 17:00:14 +0700

    create sign in user nam fragment

---
+++ app/src/main/java/com/example/movieapp/view/SIgnUpUserNameFragment.java
@@ -0,0 +1,24 @@
+package com.example.movieapp.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.movieapp.R;
+
+public class SIgnUpUserNameFragment extends Fragment {
+    public SIgnUpUserNameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_s_ign_up_user_name, container, false);
+    }
+}

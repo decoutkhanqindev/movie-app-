32e299f666bd499db4d5029d0405dfc63768b99c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-23 16:31:38 +0700

    create viewmodel to get banners, top movies and up coming movies from repository

---
+++ app/src/main/java/com/example/movieapp/viewmodel/FilmViewModel.java
@@ -0,0 +1,36 @@
+package com.example.movieapp.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.movieapp.model.Film;
+import com.example.movieapp.model.FilmRepository;
+import com.example.movieapp.model.SliderItem;
+
+import java.util.ArrayList;
+
+public class FilmViewModel extends ViewModel {
+    private final FilmRepository filmRepository;
+    private final MutableLiveData<ArrayList<SliderItem>> sliderItemsMultiLiveData;
+    private final MutableLiveData<ArrayList<Film>> topMoviesMutableLiveData;
+    private final MutableLiveData<ArrayList<Film>> upComingMutableLiveData;
+
+    public FilmViewModel() {
+        filmRepository = FilmRepository.getInstance();
+        sliderItemsMultiLiveData = filmRepository.getSliderItems();
+        topMoviesMutableLiveData = filmRepository.getTopMovies();
+        upComingMutableLiveData = filmRepository.getUpComing();
+    }
+
+    public MutableLiveData<ArrayList<Film>> getTopMovies(){
+        return topMoviesMutableLiveData;
+    }
+
+    public MutableLiveData<ArrayList<Film>> getUpComing(){
+        return upComingMutableLiveData;
+    }
+
+    public MutableLiveData<ArrayList<SliderItem>> getSliderItems(){
+        return sliderItemsMultiLiveData;
+    }
+}

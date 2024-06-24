ade47ecb783d4a0b6c4e0acdaf942f52508f7029
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-24 23:23:21 +0700

    handle click each film move to each detail

--- app/src/main/java/com/example/movieapp/view/adapter/FilmListAdapter.java
+++ app/src/main/java/com/example/movieapp/view/adapter/FilmListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.movieapp.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.movieapp.R;
 import com.example.movieapp.model.Film;
+import com.example.movieapp.view.FilmDetailActivity;
 
 import java.util.ArrayList;
 
@@ -46,7 +48,9 @@ public class FilmListAdapter extends RecyclerView.Adapter<FilmListAdapter.FilmLi
         holder.filmTitle.setText(film.getTitle());
 
         holder.itemView.setOnClickListener(v -> {
-
+            Intent intent = new Intent(context, FilmDetailActivity.class);
+            intent.putExtra("Film", film);
+            context.startActivity(intent);
         });
     }
 

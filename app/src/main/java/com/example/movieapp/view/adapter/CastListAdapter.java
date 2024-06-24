d93e50cb362de2c2701b7e49d1e494240b0a1283
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-25 00:42:58 +0700

    handle binding view and data to layout cast list

--- app/src/main/java/com/example/movieapp/view/adapter/CastListAdapter.java
+++ app/src/main/java/com/example/movieapp/view/adapter/CastListAdapter.java
@@ -1,4 +1,60 @@
 package com.example.movieapp.view.adapter;
 
-public class CastListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.movieapp.R;
+import com.example.movieapp.model.Cast;
+
+import java.util.ArrayList;
+
+public class CastListAdapter extends RecyclerView.Adapter<CastListAdapter.CastViewHolder> {
+    private Context context;
+    private ArrayList<Cast> castArrayList;
+
+    public CastListAdapter(ArrayList<Cast> castArrayList, Context context) {
+        this.castArrayList = castArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CastViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cast_viewholder, parent, false);
+        return new CastViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CastViewHolder holder, int position) {
+        Cast cast = castArrayList.get(position);
+
+        Glide.with(context).load(cast.getPicUrl()).into(holder.castImg);
+
+        holder.castName.setText(cast.getActor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return castArrayList.size();
+    }
+
+
+    public static class CastViewHolder extends RecyclerView.ViewHolder{
+        private ImageView castImg;
+        private TextView castName;
+
+        public CastViewHolder(@NonNull View itemView) {
+            super(itemView);
+            castImg = itemView.findViewById(R.id.castImg);
+            castName = itemView.findViewById(R.id.castName);
+        }
+    }
 }

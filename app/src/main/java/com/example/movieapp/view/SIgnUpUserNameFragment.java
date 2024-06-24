dc8f94a3f9bdf4a01b37a7ffefb76b162761e97b
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-24 22:17:18 +0700

    create fragment update username

--- app/src/main/java/com/example/movieapp/view/SIgnUpUserNameFragment.java
+++ app/src/main/java/com/example/movieapp/view/SIgnUpUserNameFragment.java
@@ -1,21 +1,40 @@
 package com.example.movieapp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.movieapp.databinding.FragmentSIgnUpUserNameBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SIgnUpUserNameFragment extends Fragment {
     private FragmentSIgnUpUserNameBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
 
     public SIgnUpUserNameFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
     }
 
     @Override
@@ -23,7 +42,41 @@ public class SIgnUpUserNameFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentSIgnUpUserNameBinding.inflate(inflater, container, false);
 
+        binding.signInUserNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserName();
+            }
+        });
 
         return binding.getRoot();
+    }
+
+    private void hideFragment() {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.popBackStack();
+    }
+
+    private void updateUserName() {
+        String userName = binding.inputUserName.getText().toString();
+
+        if (!TextUtils.isEmpty(userName)){
+            UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(userName).build();
+            currentUser.updateProfile(userProfileChangeRequest)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(requireContext(), "Valid Username", Toast.LENGTH_SHORT).show();
+                            hideFragment();
+                            ((HomeLauncherActivity) requireActivity()).updateUserNameAndEmail();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
